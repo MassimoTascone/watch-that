5358a695f1aaf44535c614a03d854fd4587f8076
Massimo Tascone <massimo@Massi.local>
2023-06-17 12:30:36 +0200

    details movie style + credits api call

--- src/pages/movies/[movieId].tsx
+++ src/pages/movies/[movieId].tsx
@@ -1,10 +1,12 @@
 import { Layout } from "@/components/Layout/Layout";
 import Image from "next/image";
 
-export default function MovieDetails({ movieDetails }) {
-  console.log(movieDetails);
-  const imageUrl = movieDetails?.poster_path;
-  const bgImg = `https://image.tmdb.org/t/p/original/${movieDetails.backdrop_path}`;
+export default function MovieDetails({ movieDetailsData, creditData }) {
+  console.log(movieDetailsData);
+  console.log(creditData);
+
+  const imageUrl = movieDetailsData?.poster_path;
+  const bgImg = `https://image.tmdb.org/t/p/original/${movieDetailsData.backdrop_path}`;
 
   return (
     <Layout>
@@ -23,34 +25,55 @@ export default function MovieDetails({ movieDetails }) {
             src={`https://image.tmdb.org/t/p/w500/${imageUrl}`}
             width={300}
             height={300}
-            alt={movieDetails.title}
+            alt={movieDetailsData.title}
             priority={true}
             className="rounded-lg"
           />
         </div>
         <div className="p-3 mt-10">
           <h2 className="text-3xl font-sans font-extrabold ">
-            {movieDetails.title}
+            {movieDetailsData.title}
           </h2>
           <p className="font-sm">
-            Original title: {movieDetails.original_title}
+            Original title: {movieDetailsData.original_title}
           </p>
           <div className="flex items-center gap-3 mt-10">
-            <p className="">Movie ({movieDetails.release_date}) &#x2022;</p>
-            <p>{movieDetails.runtime} min</p>
+            <p className="">Movie ({movieDetailsData.release_date}) &#x2022;</p>
+            <p>{movieDetailsData.runtime} min</p>
           </div>
-          <p className="mt-10 text-sm leading-loose">{movieDetails.overview}</p>
+          <p className="mt-10 text-sm leading-loose">
+            {movieDetailsData.overview}
+          </p>
           <div>
             <h3>Details</h3>
             <p>Genre</p>
           </div>
         </div>
         <div className="p-3 ml-20 mt-10">
-          <h2 className="font-bold text-lg">Cast and Crew:</h2>
+          <h2 className="font-bold text-2xl">Cast and Crew:</h2>
           <ul>
-            <li></li>
-            <li></li>
-            <li></li>
+            {creditData.cast.slice(0, 10).map((actor) => (
+              <li key={actor.id} className="flex items-center my-4">
+                <div className="avatar">
+                  <div className="w-12 rounded-full">
+                    <Image
+                      src={`https://image.tmdb.org/t/p/w154/${actor.profile_path}`}
+                      width={40}
+                      height={40}
+                      alt={actor.name}
+                      priority={true}
+                    />
+                  </div>
+                </div>
+                <div className="ml-3">
+                  <p className="font-bold">{actor.name}</p>
+                  <p className="text-sm font-light italic">
+                    as {actor.character}
+                  </p>
+                </div>
+              </li>
+            ))}
+            <li>...</li>
           </ul>
         </div>
       </section>
@@ -58,27 +81,55 @@ export default function MovieDetails({ movieDetails }) {
   );
 }
 
-export async function getServerSideProps(context) {
+// export async function getServerSideProps(context) {
+//   console.log(context.query);
+//   const { movieId } = context.query;
+
+//   try {
+//     const res = await fetch(
+//       `https://api.themoviedb.org/3/movie/${movieId}?api_key=${process.env.API_KEY}`
+//     );
+//     const movieDetails = await res.json();
+
+//     return {
+//       props: {
+//         movieDetails,
+//       },
+//     };
+//   } catch (error) {
+//     console.log("Error fetching movie data:", error);
+//     return {
+//       props: {
+//         movieDetails: null,
+//       },
+//     };
+//   }
+// }
+
+export const getServerSideProps = async (context) => {
   console.log(context.query);
   const { movieId } = context.query;
 
   try {
-    const res = await fetch(
-      `https://api.themoviedb.org/3/movie/${movieId}?api_key=${process.env.API_KEY}`
-    );
-    const movieDetails = await res.json();
+    const [creditRes, movieDetailsRes] = await Promise.all([
+      fetch(
+        `https://api.themoviedb.org/3/movie/${movieId}/credits?api_key=${process.env.API_KEY}`
+      ),
+      fetch(
+        `https://api.themoviedb.org/3/movie/${movieId}?api_key=${process.env.API_KEY}`
+      ),
+    ]);
 
-    return {
-      props: {
-        movieDetails,
-      },
-    };
+    const creditData = await creditRes.json();
+    const movieDetailsData = await movieDetailsRes.json();
+
+    console.log({ creditData, movieDetailsData });
+
+    return { props: { creditData, movieDetailsData } };
   } catch (error) {
     console.log("Error fetching movie data:", error);
     return {
-      props: {
-        movieData: null,
-      },
+      props: { creditData: null, movieDetailsData: null },
     };
   }
-}
+};
