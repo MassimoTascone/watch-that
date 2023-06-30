4ea36334bf327345c2423877b42fb1cb795019e4
Massimo Tascone <massimo@Massi.local>
2023-06-30 15:11:25 +0200

    img size fix

--- src/components/HeroBanner.tsx
+++ src/components/HeroBanner.tsx
@@ -9,10 +9,10 @@ export function HeroBanner({ moviesData }: HeroBannerProps) {
   const bannerImg = `https://image.tmdb.org/t/p/original//nniZPBIfrep9wbx0l1529RHXeD8.jpg`;
 
   return (
-    <section className="h-auto mb-60">
+    <section className="h-auto mb-10 lg:mb-60">
       <Image
         id="heroImg"
-        className="absolute top-0 left-0 -z-10 w-screen brightness-40 h-[780px]"
+        className="absolute top-0 left-0 -z-10 w-screen brightness-40 h-auto"
         src={bannerImg}
         width={1080}
         height={920}
