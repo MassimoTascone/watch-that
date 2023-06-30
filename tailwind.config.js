f1d31ed2b31e20119de327808e73722978718cc2
Massimo Tascone <massimo@Massi.local>
2023-06-30 12:07:59 +0200

    fixed dark only theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,5 +4,9 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   plugins: [require("daisyui")],
+  daisyui: {
+    themes: ["dark"], // true: all themes | false: only light + dark | array: specific themes like this ["light", "dark", "cupcake"]
+    darkTheme: "dark", // name of one of the included themes for dark mode
+  },
   theme: {},
 };
